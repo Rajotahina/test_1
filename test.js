5542f9812cf0372be8884f5de1fdce0c3e3bb369
rajotahina <rajotahina22@gmail.com>
2022-08-19 07:54:22 +0200

    premier test

---
+++ test.js
@@ -0,0 +1,15 @@
+let tab =[2,6,3,4,9,10];
+//  for(let i=0; i>tab.length ; i++){
+    
+//     console.log(tab[i]);
+//  }
+//  function showlast(tab ){
+//      for(i=0; i<tab.length ;i++ ){
+
+//      }
+//  }
+for ( let i=tab.length-1 ; i>=0 ; i--){
+    console.log(tab[i]);
+    
+    
+}
